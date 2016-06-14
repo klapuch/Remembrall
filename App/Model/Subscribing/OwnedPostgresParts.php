ae11dceb9a1ba11af29db71ea87ce315ef1b579f
Klapuch <klapuchdominik@gmail.com>
2016-06-14 21:35:14 +0200

    added OwnedPostgresParts

---
+++ App/Model/Subscribing/OwnedPostgresParts.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types = 1);
+namespace Remembrall\Model\Subscribing;
+
+use Dibi;
+use Nette\Security;
+use Remembrall\Exception;
+
+final class OwnedPostgresParts implements Parts {
+	private $database;
+	private $page;
+	private $myself;
+
+	public function __construct(
+		Dibi\Connection $database,
+		Page $page,
+		Security\IIdentity $myself
+	) {
+		$this->database = $database;
+		$this->page = $page;
+		$this->myself = $myself;
+	}
+
+	public function subscribe(Part $part, Interval $interval) {
+		try {
+			$this->database->query(
+				'INSERT INTO parts
+				(url, expression, content, visited_at, `interval`, user_id) VALUES
+				(?, ?, ?, ?, ?, ?)',
+				$this->page->url(),
+				(string)$part->expression(),
+				$part->content(),
+				$interval->start(),
+				$interval->step()->i,
+				$this->myself->getId()
+			);
+		} catch(Dibi\UniqueConstraintViolationException $ex) {
+			throw new Exception\DuplicateException(
+				sprintf(
+					'"%s" expression on the "%s" page is already subscribed by you',
+					(string)$part->expression(),
+					$this->page->url()
+				),
+				$ex->getCode(),
+				$ex
+			);
+		}
+	}
+
+	public function iterate(): array {
+		return (array)array_reduce(
+			$this->database->fetchAll(
+				'SELECT content, expression
+				FROM parts
+				WHERE url = ? AND user_id = ?',
+				$this->page->url(),
+				$this->myself->getId()
+			),
+			function($previous, array $row) {
+				$previous[] = new ConstantPart(
+					$this->page,
+					$row['content'],
+					new XPathExpression($this->page, $row['expression'])
+				);
+				return $previous;
+			}
+		);
+	}
+}
