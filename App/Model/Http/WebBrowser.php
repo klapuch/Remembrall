7fb0b735dfea31de739e826bd418a0dd2d31ec28
Facedown <klapuchdominik@gmail.com>
2016-06-29 21:33:48 +0200

    Checked unknown url

--- App/Model/Http/WebBrowser.php
+++ App/Model/Http/WebBrowser.php
@@ -3,7 +3,9 @@ declare(strict_types = 1);
 namespace Remembrall\Model\Http;
 
 use GuzzleHttp;
+use GuzzleHttp\Exception\ConnectException;
 use Psr\Http\Message;
+use Remembrall\Exception\ExistenceException;
 
 final class WebBrowser implements Browser {
 	private $http;
@@ -13,19 +15,27 @@ final class WebBrowser implements Browser {
 	}
 
 	public function send(Request $request): Response {
-		$headers = $request->headers();
-		return new DefaultResponse(
-			$this->http->request(
-				$headers->header('method')->value(),
-				$headers->header('host')->value(),
-				array_reduce(
-					$headers->iterate(),
-					function($previous, Header $header) {
-						$previous[$header->field()] = $header->value();
-						return $previous;
-					}
+		try {
+			$headers = $request->headers();
+			return new DefaultResponse(
+				$this->http->request(
+					$headers->header('method')->value(),
+					$headers->header('host')->value(),
+					array_reduce(
+						$headers->iterate(),
+						function($previous, Header $header) {
+							$previous[$header->field()] = $header->value();
+							return $previous;
+						}
+					)
 				)
-			)
-		);
+			);
+		} catch(ConnectException $ex) {
+			throw new ExistenceException(
+				'Given URL does not exist',
+				$ex->getCode(),
+				$ex
+			);
+		}
 	}
 }
