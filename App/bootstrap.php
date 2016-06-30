153ef2c18635a2f1cc2b08546aceadfbb0abc8ff
Facedown <klapuchdominik@gmail.com>
2016-06-30 21:48:13 +0200

    updated composer and added js

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -3,8 +3,7 @@ declare(strict_types = 1);
 require __DIR__ . '/../vendor/autoload.php';
 $configuration = new Nette\Configurator();
 $configuration->enableDebugger(__DIR__ . '/../Log');
-return $configuration->setDebugMode(true)
-	->setTempDirectory(__DIR__ . '/../Temporary')
+return $configuration->setTempDirectory(__DIR__ . '/../Temporary')
 	->addConfig(__DIR__ . '/Configuration/config.neon')
 	->addConfig(__DIR__ . '/Configuration/config.production.neon')
 	->createContainer();
