6684d064efccdadee7bf7b2582c37df772a3c1b9
Dominik Klapuch <klapuchdominik@gmail.com>
2016-09-19 22:46:39 +0200

    Formatting

--- App/Model/Subscribing/HtmlWebPage.php
+++ App/Model/Subscribing/HtmlWebPage.php
@@ -10,31 +10,31 @@ use Klapuch\Http;
  */
 final class HtmlWebPage implements Page {
     const CONTENT_TYPE = 'text/html';
-	private $request;
+    private $request;
 
-	public function __construct(Http\Request $request) {
-		$this->request = $request;
-	}
+    public function __construct(Http\Request $request) {
+        $this->request = $request;
+    }
 
-	public function content(): \DOMDocument {
+    public function content(): \DOMDocument {
         try {
             $response = new Http\StrictResponse(
                 ['Content-Type' => self::CONTENT_TYPE],
                 new Http\AvailableResponse($this->request->send())
             );
-			$dom = new DOM();
-			$dom->loadHTML($response->body());
-			return $dom;
-		} catch(\Exception $ex) {
-			throw new NotFoundException(
+            $dom = new DOM();
+            $dom->loadHTML($response->body());
+            return $dom;
+        } catch(\Exception $ex) {
+            throw new NotFoundException(
                 'Page is unreachable. Does the URL exist?',
-				$ex->getCode(),
-				$ex
-			);
-		}
-	}
+                $ex->getCode(),
+                $ex
+            );
+        }
+    }
 
-	public function refresh(): Page {
-		return new self($this->request);
-	}
+    public function refresh(): Page {
+        return new self($this->request);
+    }
 }
