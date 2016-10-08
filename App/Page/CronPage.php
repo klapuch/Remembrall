03cbd36281f4c08f8c97bdd8ecec355543b5eba4
Dominik Klapuch <klapuchdominik@gmail.com>
2016-10-08 23:28:40 +0200

    Added cron page

--- App/Page/CronPage.php
+++ App/Page/CronPage.php
@@ -3,15 +3,41 @@ declare(strict_types = 1);
 namespace Remembrall\Page;
 
 use Nette\Mail;
-use Remembrall\Model\{
-	Email
-};
+use Remembrall\Model\Subscribing;
 
 final class CronPage extends BasePage {
 	/** @var Mail\IMailer @inject */
 	public $mailer;
 
 	public function actionDefault() {
-		echo 'OK';
+		try {
+			$parts = (new Subscribing\LoggedParts(
+				new Subscribing\UnreliableParts(
+					new Subscribing\CollectiveParts($this->database),
+					$this->database
+				),
+				$this->logger
+			))->iterate();
+			/** @var Subscribing\Part $part */
+			foreach($parts as $part)
+				$part->refresh();
+			$subscriptions = (new Subscribing\LoggedSubscriptions(
+				new Subscribing\ChangedSubscriptions(
+					new Subscribing\OwnedSubscriptions(
+						$this->subscriber,
+						$this->database
+					),
+					$this->mailer,
+					$this->database
+				),
+				$this->logger
+			))->iterate();
+			/** @var Subscribing\Subscription $subscription */
+			foreach($subscriptions as $subscription)
+				$subscription->notify();
+			echo 'OK';
+		} catch(\Throwable $ex) {
+			echo $ex->getMessage();
+		}
 	}
 }
