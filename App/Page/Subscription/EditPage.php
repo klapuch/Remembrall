ed14a27315e731831f9d95bb5082521ab6e30556
Dominik Klapuch <klapuchdominik@gmail.com>
2017-03-12 13:59:31 +0100

    Edit page

---
+++ App/Page/Subscription/EditPage.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+namespace Remembrall\Page\Subscription;
+
+use Klapuch\Output;
+use Klapuch\Time;
+use Remembrall\Control\Subscription;
+use Remembrall\Model\Subscribing;
+use Remembrall\Page;
+
+final class EditPage extends Page\BasePage {
+	public function render(array $parameters): Output\Format {
+		$id = (int)$parameters[0];
+		$dom = new \DOMDocument();
+		$dom->loadXML(
+			sprintf(
+				'<forms>%s</forms>',
+				(new Subscription\EditForm(
+					$this->subscription($id),
+					$this->url,
+					$this->csrf,
+					$this->backup
+				))->render()
+			)
+		);
+		return new Output\DomFormat($dom, 'xml');
+	}
+
+	public function submitEdit(array $subscription, array $parameters): void {
+		try {
+			$id = (int)$parameters[0];
+			(new Subscription\EditForm(
+				$this->subscription($id),
+				$this->url,
+				$this->csrf,
+				$this->backup
+			))->submit(function() use($subscription, $id) {
+				(new Subscribing\StoredSubscription(
+					$id, $this->database
+				))->edit(
+					new Time\TimeInterval(
+						new \DateTimeImmutable(),
+						new \DateInterval(
+							sprintf('PT%dM', $subscription['interval'])
+						)
+					)
+				);
+			});
+			$this->flashMessage('Subscription has been edited', 'success');
+			$this->redirect('subscriptions');
+		} catch(\Throwable $ex) {
+			$this->flashMessage($ex->getMessage(), 'danger');
+			$this->redirect($this->url->path());
+		}
+	}
+
+	private function subscription(int $id): Subscribing\Subscription {
+		return new Subscribing\OwnedSubscription(
+			new Subscribing\StoredSubscription($id, $this->database),
+			$id,
+			$this->user,
+			$this->database
+		);
+	}
+}
---
+++ App/Page/Subscription/templates/edit.xsl
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+
+	<xsl:import href="../../templates/@layout.xsl"/>
+	<xsl:import href="../../components/form.xsl"/>
+
+	<xsl:template match="page">
+		<h1><xsl:apply-templates select="body/header[@level = 1]"/></h1>
+		<xsl:apply-templates select="forms/form[@name='edit']"/>
+	</xsl:template>
+
+</xsl:stylesheet>
