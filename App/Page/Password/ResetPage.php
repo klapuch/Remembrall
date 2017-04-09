3ac1724df124cb395842e9d359205157119f7179
Dominik Klapuch <klapuchdominik@gmail.com>
2017-04-09 15:59:07 +0200

    Added password reset

---
+++ App/Page/Password/ResetPage.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types = 1);
+namespace Remembrall\Page\Password;
+
+use Klapuch\Access;
+use Klapuch\Encryption;
+use Klapuch\Output;
+use Remembrall\Control\Password;
+use Remembrall\Page;
+
+final class ResetPage extends Page\BasePage {
+	public function render(array $parameters): Output\Format {
+		$dom = new \DOMDocument();
+		$dom->loadXML(
+			sprintf(
+				'<forms>%s</forms>',
+				(new Password\ResetForm(
+					$parameters['reminder'],
+					$this->url,
+					$this->csrf,
+					$this->backup
+				))->render()
+			)
+		);
+		return new Output\DomFormat($dom, 'xml');
+	}
+
+	public function submitReset(array $credentials): void {
+		try {
+			(new Password\ResetForm(
+				$credentials['reminder'],
+				$this->url,
+				$this->csrf,
+				$this->backup
+			))->submit(function() use ($credentials) {
+				(new Access\ExpirableRemindedPassword(
+					$credentials['reminder'],
+					$this->database,
+					new Access\RemindedPassword(
+						$credentials['reminder'],
+						$this->database,
+						new Access\UserPassword(
+							new Access\ForgetfulUser(
+								$credentials['reminder'],
+								$this->database
+							),
+							$this->database,
+							new Encryption\AES256CBC(
+								$this->configuration['KEYS']['password']
+							)
+						)
+					)
+				))->change($credentials['password']);
+			});
+			$this->flashMessage('Password has been reset', 'success');
+			$this->redirect('sign/in');
+		} catch (\Throwable $ex) {
+			$this->flashMessage($ex->getMessage(), 'danger');
+			$this->redirect('password/remind');
+		}
+	}
+}
