1e92b03cec3d7af9cf8d6599332911a01612c331
Dominik Klapuch <klapuchdominik@gmail.com>
2017-05-19 20:08:34 +0200

    Renamed build to after-deploy

--- rsync_travis.sh
+++ rsync_travis.sh
@@ -5,4 +5,4 @@ rsync -rtilpzcvh \
     --rsync-path='sudo rsync' \
     --exclude-from="$TRAVIS_BUILD_DIR/.rsync_ignore" \
     --delete \
-    $TRAVIS_BUILD_DIR root@81.95.108.74:"$PROJECTS && $TARGET/vendor/bin/phing -f $TARGET/build.xml build"
+    $TRAVIS_BUILD_DIR root@81.95.108.74:"$PROJECTS && $TARGET/vendor/bin/phing -f $TARGET/build.xml after-deploy"
