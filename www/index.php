eddf4ee9216799f6868e5dbef8460624acc10bc9
Dominik Klapuch <klapuchdominik@gmail.com>
2017-08-12 15:24:51 +0200

    Separated API and WEB routes (#61)

--- www/index.php
+++ www/index.php
@@ -10,7 +10,8 @@ use Klapuch\Uri;
 
 const CONFIGURATION = __DIR__ . '/../App/Configuration/.config.ini',
 	LOCAL_CONFIGURATION = __DIR__ . '/../App/Configuration/.config.local.ini',
-	ROUTES = __DIR__ . '/../App/Configuration/routes.json',
+	WEB_ROUTES = __DIR__ . '/../App/Configuration/Routes/web.json',
+	API_ROUTES = __DIR__ . '/../App/Configuration/Routes/api.json',
 	LOGS = __DIR__ . '/../log';
 echo new Application\SuitedPage(
 	new Ini\CombinedSource(
@@ -23,7 +24,10 @@ echo new Application\SuitedPage(
 		)
 	),
 	new Log\FilesystemLogs(new Log\DynamicLocation(new Log\DirectoryLocation(LOGS))),
-	new Routing\HttpRoutes(json_decode(file_get_contents(ROUTES), true), $_SERVER['REQUEST_METHOD']),
+	new Routing\HttpRoutes(
+		json_decode(file_get_contents(WEB_ROUTES), true) + json_decode(file_get_contents(API_ROUTES), true),
+		$_SERVER['REQUEST_METHOD']
+	),
 	new Uri\BaseUrl(
 		$_SERVER['SCRIPT_NAME'],
 		$_SERVER['REQUEST_URI'],
